6271a59955ec2427ea7b271cfe8f2e926ae8c85f
Phoompirak <rakpumpum@gmail.com>
2025-03-16 17:59:29 +0700

    Fix id Video for scroll on click navbar

--- app/components/VideoShow.tsx
+++ app/components/VideoShow.tsx
@@ -12,7 +12,7 @@ const linkYT = [
 
 const VideoShow = () => {
   return (
-    <div id="Video Editor" className="bg-[#0a0a0a] text-white flex flex-col p-14 gap-10">
+    <div id="Video" className="bg-[#0a0a0a] text-white flex flex-col p-14 gap-10">
       <h1 className="text-3xl font-bold mb-6 text-center">Video Editor</h1>
 
       <ul className="list-none grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-10">
