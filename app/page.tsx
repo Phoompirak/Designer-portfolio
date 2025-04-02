3a23281381b09603b5b47d24cef8a659203a72a7
Phoompirak <rakpumpum@gmail.com>
2025-04-02 13:16:39 +0700

    fix background black

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex overflow-hidden flex-col min-h-screen justify-center items-center bg-[#0a0a0a]">
+    <div className="w-full">
       <div className="fixed top-0 left-0 right-0 z-[100]">
         <Navbar />
       </div>
